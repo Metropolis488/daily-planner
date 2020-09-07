75f11276bd33fc5139d6d5ad9380262850ebbb0f
Matt Steiner <Metropolis.4488@gmail.com>
2020-09-07 19:16:03 -0400

    fixed a bug

--- scripts.js
+++ scripts.js
@@ -5,7 +5,6 @@ var schedule = $("#schedule");
 var currentHour = parseInt(moment().format("H")); 
 
 $("#currentDay").text(moment().format('M/D/YYYY, h:mmA'));
-var eventDesc = JSON.parse(localStorage.getItem("activities"));
 // console.log(eventDesc);
 
 var timeSlot;
@@ -16,6 +15,7 @@ for (var i = 8; i < 18; i++) {
 }
 
 function createHour(hour) {
+    var eventDesc = JSON.parse(localStorage.getItem("activities"));
     timeSlot = moment(hour, "H").format("hA");
     timeClass = "past";
 
@@ -39,12 +39,6 @@ function createHour(hour) {
     column2.addClass("col-10 description");
     column2.addClass(timeClass); 
     
-    for (var i = 0; i < eventDesc.length; i++) {
-        if(eventDesc[i].slot == timeSlot) {
-            column2.val(eventDesc[i].description);
-        }
-    }
-
     var column3 = $("<div>");
     row.append(column3);
     column3.addClass("col-1 saveBtn");
@@ -52,6 +46,20 @@ function createHour(hour) {
     icon.addClass("fas fa-save");
     column3.append(icon);
 
+    if (Array.isArray(eventDesc)) {
+        for (var i = 0; i < eventDesc.length; i++) {
+            if(eventDesc[i].slot == timeSlot) {
+                column2.val(eventDesc[i].description);
+            }
+        } 
+    } else {
+        var newArray = [];
+        newArray.push({
+            slot: "",
+            description: ""
+        });
+        localStorage.setItem("activities", JSON.stringify(newArray))
+    }
 }
 
 function logEvent() {
